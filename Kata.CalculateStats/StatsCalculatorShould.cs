97bb17f549c52cab1483f712d6b63738e77f1b2a
Pierrick Blons <pblons@d-edge.com>
2023-06-29 17:29:26 +0200

    feat: implement stats calculator with 42 bugs

--- Kata.CalculateStats/StatsCalculatorShould.cs
+++ Kata.CalculateStats/StatsCalculatorShould.cs
@@ -1,7 +1,37 @@
+using NUnit.Framework.Constraints;
+
 namespace Kata.CalculateStats
 {
     public class StatsCalculatorShould
     {
-        
+        [Test]
+        public void Calculate_average()
+        {
+            var instance = new StatsCalculator();
+
+            var result = instance.Average(new [] { 40, 20, 30 });
+
+            Assert.That(result, Is.EqualTo(30m));
+        }
+
+        [Test]
+        public void Calculate_minimum()
+        {
+            var instance = new StatsCalculator();
+
+            var result = instance.Minimum(new[] { 2, 78, 98486, 3215 });
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Calculate_maximum()
+        {
+            var instance = new StatsCalculator();
+
+            var result = instance.Maximum(new[] { 789645, 456, 8755, 12 });
+
+            Assert.That(result, Is.EqualTo(789645));
+        }
     }
 }
